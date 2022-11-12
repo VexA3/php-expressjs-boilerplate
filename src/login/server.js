f2cf17263283b22559f1066cafda7909922a6b0e
Billy <47019848+VexA3@users.noreply.github.com>
2022-11-12 14:51:03 -0600

    Setup basic server.js

---
+++ src/login/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const host = "0.0.0.0";
+const port = 9000;
+
+app.get('/', (req, res) => {
+    res.send("Express Server listening on poart 9000");
+});
+
+app.listen(port, host, () => {
+    console.log('Running on http://%s:%s', host, port);
+});
